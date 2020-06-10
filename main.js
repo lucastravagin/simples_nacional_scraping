1e0bd4a3ef3859487cf9203993e4e97c96df19ce
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-06-10 18:10:14 -0300

    subiu

--- main.js
+++ main.js
@@ -1,105 +1,36 @@
-const puppeter = require('puppeteer')
+const request = require('request');
 const app = require('express')()
-const port = 2000
-const url = 'https://consopt.www8.receita.fazenda.gov.br/consultaoptantes'
+const port = 2000;
+
 
 app.listen(port, function () {
-    console.log(`app listen on port ${port}`)
+    console.log(`app listening on port ${port}`)
 })
+
 
 app.get('/', async (req, res) => {
-    try {
-        var cnpj = req.query.cnpj
+    var cnpj = req.query.cnpj
+    var token = '7012C9B1-4323-44F2-9185-0C3BDFB29A72'
 
-        // await scrape(cnpj).then((value)=>{
-            //if (value == undefined) res.status(500).send({Erro: 'Erro no processamento da requisção, tente novamente'})
-            res.status(200).json({
-                "cnpj": " 00.926.549/0001-01",
-                "empresa": "A  A COLEGIO ESTADUAL CORONEL SERGIO JOSE DO AMARAL",
-                "simples_nacional": "NÃO optante pelo Simples Nacional",
-                "simei": "NÃO enquadrado no SIMEI"
-            })
-        // })
-        
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({Erro: 'CNPJ não encontrado'})
+    let optionsCNPJ = {
+        'method': 'GET',
+        'url': `https://www.sintegraws.com.br/api/v1/execute-api.php?token=${token}&cnpj=${cnpj}&plugin=SN`
     }
+    let registros = await getRegistros(optionsCNPJ)
+
+    res.status(200).send(registros);
 
 })
 
-const chromeOptions = {
-    headless: true,
-    defaultViewport: null,
-    args: ['--enable-features=NetworkService'],
-    ignoreHTTPSErrors: true
-}
-
-
-
-// {
-//     args: ['--enable-features=NetworkService'],
-//     headless: true,
-//     ignoreHTTPSErrors: true,
-//   }
-let scrape = async (cnpj) => {
-
-    try {
-        const browser = await puppeter.launch(chromeOptions)
-        const page = await browser.newPage()
-        await page.goto(url, { waitUntil: 'networkidle2' })
-        ////page.setIgnoreHTTPSErrors(true);
-
-
-        await page.type('#Cnpj', cnpj, {delay: 200}) 
-
-        await page.evaluate(async () => {
-            let botao = document.getElementById('btnSubmit')
-            botao.click()
-            await new Promise(r => setTimeout(r, 2000));
-            const elementErro = document.querySelector("#conteudoPage > div:nth-child(2) > form > div.alert.alert-danger")
-            if (elementErro) {
-                return botao.click()
+const getRegistros = (optionsCNPJ) => {
+    return new Promise((resolve, reject) => {
+        request(optionsCNPJ, (error, response) => {
+            if (error) return reject(err);
+            try {
+                resolve(JSON.parse(response.body))
+            } catch (error) {
+                reject(error)
             }
-              
         })
-
-      
-        // await page.waitFor(2000)
-        // await page.evaluate(() => {
-        //     document.getElementById('btnMaisInfo').click()
-        // })
-
-        await page.waitFor(2000)
-        const result = await page.evaluate(() => {
-            let registros = {}
-            registros.cnpj = document.querySelector("#conteudo > div:nth-child(2) > div.panel-body > span:nth-child(1)").textContent
-            registros.empresa = document.querySelector("#conteudo > div:nth-child(2) > div.panel-body > span:nth-child(6)").textContent
-            registros.simples_nacional = document.querySelector("#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(1)").textContent
-            registros.simei = document.querySelector("#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(3)").textContent
-            // registros.opcao_pelo_simples_nacional_anteriores = document.querySelector("#maisInfo > div:nth-child(2) > div.panel-body > span:nth-child(1) > span").textContent
-            // registros.enquadramentos_simei_anteriores = document.querySelector("#maisInfo > div:nth-child(2) > div.panel-body > span:nth-child(4) > span").textContent
-            // registros.eventos_futuros_simples_nacional = document.querySelector("#spnEvFutSimei").textContent
-            // registros.eventos_futuros_simei = document.querySelector("#spnEvFutSimei").textContent
-            return registros
-        })
-
-        //await page.waitForSelector('#btnMaisInfo')
-        // await page.waitFor(4000)
-        // await page.evaluate(() => {
-        //     document.getElementById('btnMaisInfo').click()
-        // })
-
-        browser.close()
-        return result
-
-    } catch (error) {
-        console.log(error)
-    }
+    })
 }
-
-
-
-
-
-
---
+++ main2.js
@@ -0,0 +1,112 @@
+const puppeter = require('puppeteer')
+const app = require('express')()
+const port = 2000
+const url = 'https://consopt.www8.receita.fazenda.gov.br/consultaoptantes'
+
+app.listen(port, function () {
+    console.log(`app listen on port ${port}`)
+})
+
+app.get('/', async (req, res) => {
+    try {
+        var cnpj = req.query.cnpj
+
+         await scrape(cnpj).then((value)=>{
+            if (value == undefined) res.status(500).send({Erro: 'Erro no processamento da requisção, tente novamente'})
+            res.status(200).json(value)
+         })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({Erro: 'CNPJ não encontrado'})
+    }
+
+})
+
+const chromeOptions = {
+    headless: false,
+    defaultViewport: null,
+    args: ['--enable-features=NetworkService'],
+    ignoreHTTPSErrors: true
+}
+
+
+
+// {
+//     args: ['--enable-features=NetworkService'],
+//     headless: true,
+//     ignoreHTTPSErrors: true,
+//   }
+let scrape = async (cnpj) => {
+
+    try {
+        const browser = await puppeter.launch(chromeOptions)
+        const page = await browser.newPage()
+        await page.goto(url, { waitUntil: 'networkidle2' })
+        ////page.setIgnoreHTTPSErrors(true);
+
+
+//         await page.type('#Cnpj', cnpj, {delay: 200}) 
+
+//         await page.waitFor(3000)
+//         await page.evaluate(async () => {
+//             let botao = document.getElementById('btnSubmit')
+//             botao.click()
+//             //await new Promise(r => setTimeout(r, 2000))
+//             //const elementErro = document.querySelector("#conteudoPage > div:nth-child(2) > form > div.alert.alert-danger")
+//            // if (elementErro) {
+//                 return botao.click()
+//            // }
+              
+//         })
+
+      
+//         await page.waitFor(5000)
+//         await page.evaluate(() => {
+//             let botao = document.getElementById('btnSubmit')
+//              if (botao) {
+//                 return botao.click()
+//             }
+//         })
+
+//         await page.waitFor(5000)
+//         await page.evaluate(() => {
+//             let botao = document.getElementById('btnSubmit')
+//              if (botao) {
+//                 return botao.click()
+//             }
+//         })
+
+//         await page.waitFor(2000)
+//         const result = await page.evaluate(() => {
+//             let registros = {}
+//             registros.cnpj = document.querySelector("#conteudo > div:nth-child(2) > div.panel-body > span:nth-child(1)").textContent
+//             registros.empresa = document.querySelector("#conteudo > div:nth-child(2) > div.panel-body > span:nth-child(6)").textContent
+//             registros.simples_nacional = document.querySelector("#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(1)").textContent
+//             registros.simei = document.querySelector("#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(3)").textContent
+//             // registros.opcao_pelo_simples_nacional_anteriores = document.querySelector("#maisInfo > div:nth-child(2) > div.panel-body > span:nth-child(1) > span").textContent
+//             // registros.enquadramentos_simei_anteriores = document.querySelector("#maisInfo > div:nth-child(2) > div.panel-body > span:nth-child(4) > span").textContent
+//             // registros.eventos_futuros_simples_nacional = document.querySelector("#spnEvFutSimei").textContent
+//             // registros.eventos_futuros_simei = document.querySelector("#spnEvFutSimei").textContent
+//             return registros
+//         })
+
+//         //await page.waitForSelector('#btnMaisInfo')
+//         // await page.waitFor(4000)
+//         // await page.evaluate(() => {
+//         //     document.getElementById('btnMaisInfo').click()
+//         // })
+
+//         browser.close()
+//         return result
+
+    } catch (error) {
+         console.log(error)
+     }
+ }
+
+
+
+
+
+
